b7b5db0544e16e7d5d6144c503319a306faf40b1
Andrei Vasilevich <storymail444@gmail.com>
2020-04-28 21:52:19 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        mavenLocal()
+        jcenter()
+        gradlePluginPortal()
+        maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
+        maven { url = uri("https://kotlin.bintray.com/ktor") }
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://dl.bintray.com/spekframework/spek-dev") }
+        maven { url = uri("https://kotlin.bintray.com/kotlin-eap") }
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+    }
+}
+
+rootProject.name = rootProject.projectDir.name
+
+include(
+    "common",
+    "login-server",
+    "game-server",
+    "chat-server"
+)
