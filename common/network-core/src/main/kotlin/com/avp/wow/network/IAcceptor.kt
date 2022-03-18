ef9db93e4594ca8880b1d3873c980022c66603d5
andrei.vasilevich <Andrei.Vasilevich@totalenergies.be>
2022-03-19 01:55:01 +0600

    deletes

--- common/network-core/src/main/kotlin/com/avp/wow/network/IAcceptor.kt
+++
@@ -1,7 +0,0 @@
-package com.avp.wow.network
-
-import java.nio.channels.SelectionKey
-
-interface IAcceptor {
-    fun accept(key: SelectionKey)
-}
--- common/network-ktx/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-group = "com.avp.wow.network-ktx"
-
-dependencies {
-
-    api(project(":network-core"))
-    testImplementation(project(":network-client"))
-
-}
